c5db17ad69194866cea2b5091d15b8ffaefc8d7e
HiramRed <1165107440@qq.com>
2023-04-01 19:09:31 +0800

    Add comments and README.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # stringbuffer
 An expandable stringbuffer implemented by C language
+
+## example:
+``` c
+    stringbuf *strb = stringbuf_new(10);        // default capacity
+    stringbuf_append(strb, "Hello");            // append "Hello" into buffer
+    char * str = stringbuf_array(strb);         // return a copy of buffer
+
+    stringbuf *strb0 = string_from("Hello", 100);   // initial capacity
+```
--- stringbuf.h
+++ stringbuf.h
@@ -5,26 +5,32 @@ typedef unsigned int uint_16;
 
 typedef struct _stringbuf
 {
-    char * buf;
-    uint_16 _len;
-    uint_16 _size;
+    char * buf;         // buffer
+    uint_16 _len;       // content length
+    uint_16 _size;      // buffer memory capacity
 } stringbuf;
 
+// return a new stringbuf pointer, cap is the initial capacity of buffer
 stringbuf *
 stringbuf_new(uint_16 cap);
 
+// return a new stringbuf pointer, buf append default value, and initial capacity
 stringbuf *
 stringbuf_from(char * str, uint_16 size);
 
+// append string to buffer
 void
 stringbuf_append(stringbuf * strb, char * str, uint_16 n);
 
+// return a copy array of buffer
 char *
 stringbuf_array(stringbuf * strb);
 
+// return the length of the buffer
 uint_16
 stringbuf_len(stringbuf * strb);
 
+// free the memery resource
 void
 stringbuf_free(stringbuf * strb);
 
